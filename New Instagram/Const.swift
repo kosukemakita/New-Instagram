13de455dd1da2465605977044d8c4b8a96848d4a
kosukemakita <makitakosuke0114@gmail.com>
2020-11-03 18:38:51 +0900

    lesson 8まで（Constが定義されてない＋ログイン、アカウント作成の際にoptionalがアンラップできないというエラーが表示されている状態）

---
+++ New Instagram/Const.swift
@@ -0,0 +1,18 @@
+//
+//  Const.swift
+//  New Instagram
+//
+//  Created by 蒔田幸祐 on 2020/11/03.
+//  Copyright © 2020 kousuke.makita. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+    
+
+
+
